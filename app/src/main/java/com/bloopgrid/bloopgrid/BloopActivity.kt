3a2a1274700d491b2dbd65eeb6fe4d057dc338ca
Mark Koh <markkohdev@gmail.com>
2018-09-23 21:52:47 -0400

    Initial commit

---
+++ app/src/main/java/com/bloopgrid/bloopgrid/BloopActivity.kt
@@ -0,0 +1,12 @@
+package com.bloopgrid.bloopgrid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BloopActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bloop)
+    }
+}
